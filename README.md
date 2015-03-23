eea9e9e66a95c3c4ef39132b7a0aa043ea0e6083
Carlos Tonatihu <carlostonatihu@gmail.com>
2015-03-22 23:03:05 -0600

    Menu creado

---
+++ README.md
@@ -0,0 +1,6 @@
+# NavigationOptions
+Estas son las opciones hasta ahora y que yo conozco para hacer navegación por pestañas.
+Las opciones son:
+- Swipe Views
+- Action Bar Tabs
+- SlidingTabLayout/SlidingTabStrip
---
+++ app/src/main/java/com/ilordtony/navigationoptions/MainActivity.java
@@ -0,0 +1,38 @@
+package com.ilordtony.navigationoptions;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+    }
+
+    public void onClickButtonSwipe(View view){
+        Log.d(TAG, "onClickButtonSwipe");
+        Toast.makeText(this, "onClickButtonSwipe", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButtonTab(View view){
+        Log.d(TAG, "onClickButtonTab");
+        Toast.makeText(this, "onClickButtonTab", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButtonSliding(View view){
+        Log.d(TAG, "onClickButtonSliding");
+        Toast.makeText(this, "onClickButtonSliding", Toast.LENGTH_SHORT).show();
+    }
+
+}
